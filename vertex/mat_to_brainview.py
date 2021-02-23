286e90b2569590bb9f5dd1f0fc283d2b2387a0c4
raihaan <patelraihaan@gmail.com>
2021-02-23 11:15:17 -0500

    convert W scores back to .txt files to view on surface

---
+++ vertex/mat_to_brainview.py
@@ -0,0 +1,91 @@
+#this script takes in nmf results and writes out .txt files
+#two files are written out - one for each hemisphere listing the component scores at each vertex, and an additional column with a winner take all clustering
+#so, if you have 10 comps, expect 11 columns
+#use brainview and these outputs to visualize the spatial patterns
+
+## LOAD MODULES/SOFTWARE
+import os
+import glob
+import pandas as pd
+import numpy as np
+
+import sys
+import pickle
+import scipy
+from scipy.io import savemat, loadmat
+import scipy.stats
+
+
+#LOAD CIVET MASK TO IDENTIFY MIDLINE/ CORPOS COLLOSUM REGION
+#IDENTIFY 'VALID VERTICES' - IE VERTICES NOT IN THIS REGION
+left_mask = np.loadtxt('../surfsamp/mask_files/CIVET_2.0_mask_left_short.txt') #MODIFY point to your left mask file
+left_valid = np.where(left_mask==1) # list of valid indices in civet .txt file
+left_invalid = np.where(left_mask==0) #list of invalid indices in civet .txt file
+
+right_mask = np.loadtxt(mask_dir + "CIVET_2.0_mask_right_short.txt") #MODIFY point to your left mask file
+right_valid = (np.where(right_mask==1))
+right_invalid = (np.where(right_mask==0))
+
+fname = sys.argv[1] #.mat file containing nmf results
+out_dir=sys.argv[2]
+
+if not os.path.exists(out_dir):
+    os.mkdir(out_dir)
+    
+#load spatial components
+W = scipy.io.loadmat(fname)
+
+compnum=np.shape(W['W'])[1]
+left_W=W['W'][0:38561,:]
+right_W=W['W'][38561:,:]
+
+
+
+left_outarray = np.zeros((np.shape(left_mask)[0],compnum+1))
+
+for comp in range(0,compnum):
+    valid_idx = 0
+    for idx in range(0,np.size(left_mask)):
+        if left_mask[idx] == 1:
+            left_outarray[idx,comp] = left_W[valid_idx,comp]
+            valid_idx +=1
+
+#define winner take all clustering
+left_cluster = np.zeros((np.shape(left_W)[0],1))
+for vertex in range(0,np.shape(left_W)[0]):
+    left_cluster[vertex,0] = np.argmax(left_W[vertex,:])
+left_cluster = left_cluster + 1 #plus one for zero based indexing
+
+#now add clustering to out array, in the last column
+valid_idx=0    
+for idx in range(0,np.size(left_mask)):
+    if left_mask[idx] == 1:
+        left_outarray[idx,-1] = left_cluster[valid_idx,0]
+        valid_idx +=1
+left_statmap = out_dir + "/left_k" + str(compnum) + ".txt"
+np.savetxt(left_statmap,left_outarray)
+
+
+
+right_outarray = np.zeros((np.shape(right_mask)[0],compnum+1))
+for comp in range(0,compnum):
+    valid_idx = 0
+    for idx in range(0,np.size(right_mask)):
+        if right_mask[idx] == 1:
+            right_outarray[idx,comp] = right_W[valid_idx,comp]
+            valid_idx +=1
+
+#define winner take all clustering
+right_cluster = np.zeros((np.shape(right_W)[0],1))
+for vertex in range(0,np.shape(right_W)[0]):
+    right_cluster[vertex,0] = np.argmax(right_W[vertex,:])
+right_cluster = right_cluster + 1 #plus one for zero based indexing
+
+#now add clustering to out array, in the last column
+valid_idx=0    
+for idx in range(0,np.size(right_mask)):
+    if left_mask[idx] == 1:
+        right_outarray[idx,-1] = right_cluster[valid_idx,0]
+        valid_idx +=1
+right_statmap = out_dir + "/right_k" + str(compnum) + ".txt"
+np.savetxt(right_statmap,right_outarray)
