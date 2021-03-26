b4e5d45bf57f12e8b8d9533a48bd9366db611e2d
raihaan <patelraihaan@gmail.com>
2021-03-26 18:25:27 -0400

    add parser args, reformat accordingly

--- voxel/Hweights_tocsv.py
+++ voxel/Hweights_tocsv.py
@@ -12,11 +12,31 @@ import pickle
 import hdf5storage
 import scipy
 import scipy.stats
+import argparse
 
 #Read in csv with subject demographics 
-df_sorted = pd.read_csv('../../../raw_data/sheets/06-05-20-McGillData_WH_Exprodo-Report_IncExc_CR_CRmed_cham_CRtopfdemeduc_civetpass_slopes_time.age_sorted.csv') #MODIFY
 
-metrics=["T1T2", "DBM"] #MODIFY to be a list of the metrics used
+parser=argparse.ArgumentParser(
+    description='''This script reads in nmf results and outputs a .csv containing component weights for each subject''')
+
+group = parser.add_argument_group(title="Execution options")
+
+group.add_argument(
+    '--nmf_results', help='.mat file containing nmf results',required=True)
+group.add_argument(
+    "--metrics",help='''metrics used in nmf analysis. output will have 1 
+    column for every component metric pair''', metavar='list', nargs='+', required=True)
+group.add_argument(
+    '--demo_csv', help='demographic spreadsheet, must contain subject id',required=True)
+group.add_argument(
+    '--id_col', help='name of subject Id column in demographic sheet',required=True)
+
+args=parser.parse_args()
+
+#Read in csv with subject demographics 
+df_sorted = pd.read_csv(args.demo_csv)
+
+#metrics=["T1T2", "DBM"] #MODIFY to be a list of the metrics used
                            #the exact text used is up to you, but there should be one string for every metric
                            #the order matters - if the first chunk of data is ct, then put the ct string first
 
@@ -44,13 +64,15 @@ def append_subjweights_plsstyle(df_demo,nmf_weights, metrics):
         return
  
 #load in the nmf results of interest, check shape
-nmf_res_filename = "sample_nmf_res.mat" #MODIFY point to your nmf results
-H = hdf5storage.loadmat(nmf_res_filename)['H']
+#nmf_res_filename = "sample_nmf_res.mat" #MODIFY point to your nmf results
+H = hdf5storage.loadmat(args.nmf_results)['H']
 print(np.shape(H)) #check shape
 
 #use append_subjweights_plsstyle to concat the demographic df with the nmf weights
-df_sorted_nmfweights = append_subjweights_plsstyle(df_sorted, H)
-fname_alldata = "demographics_and_nmfweights_k10.csv" #MODIFY filename if you want
+df_sorted_nmfweights = append_subjweights_plsstyle(df_sorted, H, args.metrics)
+
+numcomps=np.shape(H)[0]
+fname_alldata = "demographics_and_nmfweights_k" + str(numcomps) + '.csv' 
 print("saving all columns to ", fname_alldata)
 df_sorted_nmfweights.to_csv(fname_alldata, index = False) 
 
@@ -58,20 +80,14 @@ df_sorted_nmfweights.to_csv(fname_alldata, index = False)
 #for pls it would be easier to have, perhaps, just subject ID and nmf weight columns. below does this
 
 #build list of column headers of interest - ie keep subject id and comp weights only for nmf csv
-comp_cols = ['Subject'] #set comp_cols to start as the non nmf columns of interest. eg this is my subject id column, so im saying i want to end up with subject ID only and component weights. add age, sex, etc, if you want
+comp_cols = [args.id_col] #set comp_cols to start as the non nmf columns of interest. eg this is my subject id column, so im saying i want to end up with subject ID only and component weights. add age, sex, etc, if you want
 for comp in range(1, np.shape(H)[0]+1):
-    for m in metrics:
+    for m in args.metrics:
         comp_cols.append("Comp" + str(comp) + "_" + m)
 
 #create a new df to write out which contains only the cols of interest
 df_nmfweights_pls = df_sorted_nmfweights[comp_cols].copy()
-fname_pls_data="Hweights_pls_k10.csv" #MODIFY filename if you want
+fname_pls_data="Hweights_k" + str(numcomps) + '.csv'
 print("saving select columns", comp_cols, "to", fname_pls_data)
 df_nmfweights_pls.to_csv(fname_pls_data, index = False) 
 
-#for pls it is also good to have a csv that is just subject ID and the cognitive data of interest
-cog_cols = ['Subject','Age','Sex','Memory','ExecFunction'] #MODIFY to be the column names of subject id and the behavioural variables of interest
-df_cog_pls = df_sorted_nmfweights[cog_cols].copy()
-fname_pls_data="pls_cogdata.csv" #MODIFY filename to appropriate name
-print("saving select columns", comp_cols, "to", fname_pls_data)
-df_cog_pls.to_csv(fname_pls_data, index = False)
