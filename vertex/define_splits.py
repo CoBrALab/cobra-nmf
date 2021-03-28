dbaf4af8be00f5ca448f68884b66cdda671f1789
raihaan <patelraihaan@gmail.com>
2021-03-28 15:16:13 -0400

    add parser args, reformat

--- vertex/define_splits.py
+++ vertex/define_splits.py
@@ -11,32 +11,54 @@ import scipy.stats
 from scipy.io import savemat, loadmat
 import sklearn
 from sklearn.model_selection import StratifiedShuffleSplit
+import argparse
+
+parser=argparse.ArgumentParser(
+    description='''This script creates stratified input matrices for stability analysis,
+    stores outputs as .mat files in stability_splits directory''')
+
+group = parser.add_argument_group(title="Execution options")
+
+group.add_argument(
+    '--demo_csv', help='demographic spreadsheet, must contain subject id',required=True)
+group.add_argument(
+    '--id_col', help='name of subject Id column in demographic sheet',required=True)
+
+group.add_argument('--n_folds', help='number of folds', type=int, default=10)
+
+group.add_argument(
+    "--inputs",help="metric matrices to stratify", metavar='list', nargs='+', required=True)
+
+group.add_argument(
+    "--stratifyby",help="demographic variables to stratify splits by", metavar='list', nargs='+', required=True)
+
+
+args=parser.parse_args()
+
+def save_mat(x,key,fname):
+    print("Saving ", np.shape(x), key, "to", fname)
+    scipy.io.savemat(fname, {'X': x})
 
 #read in demographic spreadsheet with subject ids, age, prisma etc
-df_sorted = pd.read_csv('../../../../raw_data/sheets/07-04-20-McGillData_WH_Exprodo-Report_IncExc_CR_CRmed_cham_CRtopfdemeduc_civetpass_slopes_sorted.csv') #MODIFY
+df_sorted = pd.read_csv(args.demo_csv)
 
 ## create demo matrix containing subj id and the variables to stratify by (age)
-#variables to stratify by - probably age and sex, maybe disease group as well
-age_var='OX.AGE'; sex_var= 'OX.SEX' #MODIFY
-#group_var = ?? #MODIFY if applicable, add others if applicable
 
-demo = df_sorted[['oxmg_id',age_var,sex_var]].values #MODIFY to be subjectID and the variables you want to stratify by (probably age_var, sex_var, plus others defined above)
-
-#sklearn stratify tool needs categorical vars - bin age by median val
-demo = np.hstack((demo, np.zeros((len(df_sorted),1)))) #new col for age
-median_age = np.median(df_sorted[age_var].values) 
-#<= median age -> you are young 
-demo[np.where(df_sorted[age_var].values > median_age),-1]="old"
-demo[np.where(df_sorted[age_var].values <= median_age),-1]="young"
+demo_vars = []
+demo_vars.append(args.id_col)
+for x in args.stratifyby:
+    demo_vars.append(x)
+demo = df_sorted[demo_vars].values
 
 #define train data as subj ids (x)
 #define categorical vars as vars to stratify by (y, ie labels)
 X = demo[:,0:1]
-y = demo[:,2:] #this skips [:,1] which is numerical age, instead includes categorical age
+y = demo[:,1:]
+
 
 #use sklearn Stratify tools to generate stratified splits of data
-n_splits=10 #MODIFY
-sss = StratifiedShuffleSplit(n_splits=n_splits, test_size=0.5, random_state=0)
+n_folds=args.n_folds
+sss = StratifiedShuffleSplit(n_splits=n_folds, test_size=0.5, random_state=0)
 sss.get_n_splits(X, y)
 
 Asplits_indices = {}; Asplits_subjectIDs = {}  #dicts for storing indices and corresponding subj ids
@@ -50,41 +72,37 @@ for train_index, test_index in sss.split(X, y):
     
     ID_list = []
     s = train_index[0]
-    ID_list.append(df_sorted['oxmg_id'].iloc[s]) #MODIFY oxmg_id to subject id name
+    ID_list.append(df_sorted[args.id_col].iloc[s])
     for s in train_index[1:]:
-        ID_list.append(df_sorted['oxmg_id'].iloc[s]) #MODIFY oxmg_id to subject id name
+        ID_list.append(df_sorted[args.id_col].iloc[s])
     Asplits_subjectIDs[iter] = ID_list
-    
+
     ID_list = []
     s = test_index[0]
-    ID_list.append(df_sorted['oxmg_id'].iloc[s]) #MODIFY oxmg_id to subject id name
+    ID_list.append(df_sorted[args.id_col].iloc[s])
     for s in test_index[1:]:
-        ID_list.append(df_sorted['oxmg_id'].iloc[s]) #MODIFY oxmg_id to subject id name
+        ID_list.append(df_sorted[args.id_col].iloc[s])
     Bsplits_subjectIDs[iter] = ID_list
-    
+
     iter = iter + 1
-  
+
 #save splits
 out_dir = "stability_splits/"
 if not os.path.exists(out_dir):
     os.makedirs(out_dir)
 
-pickle.dump(Asplits_indices, open( out_dir + "Asplits_indices.p", "wb"), protocol=0)
-pickle.dump(Asplits_subjectIDs, open( out_dir + "Asplits_subjectIDs.p", "wb" ))
+pickle.dump(Asplits_indices, open( out_dir + "Asplits_indices.p", "wb"))
 pickle.dump(Bsplits_indices, open( out_dir + "Bsplits_indices.p", "wb" ))
-pickle.dump(Bsplits_subjectIDs, open( out_dir + "Bsplits_subjectIDs.p", "wb" ))
 
-
-input_list=["wholebrain_t1t2","wholebrain_dbm"] #MODIFY to match the filenames of the wholebrain files created in your sample_extract*py scripts. should be name of the file without the .mat extension
+input_list = args.inputs
 
 data_dict={}
-raw_data_dir = '/folder/where/wholebrain/rawdata/stored/' #MODIFY to location of wholebrain .mat files containig raw data
-for file in input_list:
-    fname = raw_data_dir + '/' + file + ".mat"
-    data_dict[file] = scipy.io.loadmat(fname)['X'] #load raw data
+for f in input_list:
+    data_dict[f] = loadmat(f)['X'] #load raw data
+data_dict.keys()
 
 #for each split, build required indices
-for split in range(0, n_splits):
+for split in range(0, args.n_folds):
 
     #get data from first metric
     metric=input_list[0]
@@ -96,7 +114,6 @@ for split in range(0, n_splits):
     b_mx_wb = scipy.stats.zscore(data_b,axis=None)
 
     #repeat for each metric 
-    #if only one metric, skip the for loop below, go straight to line 108 #
     for metric in input_list[1:]:
         data_all = data_dict[metric]
         data_a = data_all[:,Asplits_indices[str(split)]]; data_b = data_all[:,Bsplits_indices[str(split)]]
@@ -110,11 +127,6 @@ for split in range(0, n_splits):
     b_mx_shift_wb = b_mx_wb - np.min(b_mx_wb)
 
     #write out
-    fname = out_dir + "a_" + str(split) + ".mat"
-    print(fname, np.min(a_mx_shift_wb), np.shape(a_mx_shift_wb))
-    scipy.io.savemat(fname, {'X': a_mx_shift_wb})
-
-    fname = out_dir + "b_" + str(split) + ".mat"
-    print(fname, np.min(b_mx_shift_wb), np.shape(b_mx_shift_wb))
-    scipy.io.savemat(fname, {'X': b_mx_shift_wb})
+    save_mat(a_mx_shift_wb, 'split a_' + str(split), out_dir + "a_" + str(split) + ".mat")
+    save_mat(b_mx_shift_wb, 'split b_' + str(split), out_dir + "b_" + str(split) + ".mat")
 
