c54d731f82394df3e5b689fabf8f4c1a4c66b9f4
raihaan <patelraihaan@gmail.com>
2021-03-28 15:59:14 -0400

    update with parser args, copied from ../voxel/

--- vertex/plot_Hweights.py
+++ vertex/plot_Hweights.py
@@ -6,18 +6,39 @@ import numpy as np
 
 import sys
 import pickle
+import hdf5storage
 import scipy
-from scipy.io import savemat, loadmat
 from scipy import stats
 
 import matplotlib as mpl
 from matplotlib import cm
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter, FixedFormatter, FixedLocator
 from matplotlib import pyplot as plt
-plt.switch_backend('Agg')
-fname=sys.argv[1]
-h=scipy.io.loadmat(fname)['H']
-print(np.shape(h))
+import argparse
+options = hdf5storage.Options(oned_as = 'column', matlab_compatible = True, action_for_matlab_incompatible = 'error')
+
+
+parser=argparse.ArgumentParser(
+    description='''This script outputs a .png file containing a heatmap of input nmf matrix data''')
+
+group = parser.add_argument_group(title="Execution options")
+
+group.add_argument(
+    '--nmf_weights', help='.mat file containing nmf results',required=True)
+group.add_argument(
+    '--output', help='output .png filename',required=True)
+group.add_argument(
+    '--minimum', type=float, help='min value',required=False,default=-2)
+group.add_argument(
+    '--maximum', type=float, help='max value',required=False,default=2)
+group.add_argument(
+    '--width', type=float, help='figure width',required=False,default=16)
+group.add_argument(
+    '--height', type=float, help='figure height',required=False,default=8)
+
+args=parser.parse_args()
+
+h=hdf5storage.loadmat(args.nmf_weights)['H']
 
 #heat mapping for H matrix
 def heatmapping(data,minn,maxx,cbar_tix,fig_width,fig_height,title='',fname=''):
@@ -37,23 +58,13 @@ def heatmapping(data,minn,maxx,cbar_tix,fig_width,fig_height,title='',fname=''):
     ax.set_aspect('auto') #use 'auto' for automatic aspect
     ax.tick_params(axis='both',which='both',bottom='off',top='off',labelbottom='on',left='on',labelleft='on', pad = 5)
     ax.set_xlabel('')
-    #xmajorLocator = FixedLocator([210, 630, 1050,1470,1890])
-    #xmajorFormatter = FixedFormatter(['CT','SA','MD','FA','RD'])
-    #ax.xaxis.set_major_locator(xmajorLocator)
-    #ax.xaxis.set_major_formatter(xmajorFormatter)
-    #plt.setp(ax.get_xmajorticklabels(), rotation='horizontal', fontsize=40)
-    ax.set_ylabel('Vertices', fontsize=40)
+    ax.set_ylabel('Voxels', fontsize=40)
     ax.yaxis.set_ticklabels([])
     ax.yaxis.labelpad = 5
     ax.tick_params(axis='y',size=15)
     ax.grid(False)
     fig = plt.gcf()
     fig.set_size_inches(fig_width,fig_height)
-    n_metrics = 5
-    #n_subj = np.shape(data)[1]/5
-    #for x in range(1,n_metrics):
-    #    plt.axvline(x=(n_subj*x),c='w',linewidth=2)
-    #Generate a color bar
     cbar = plt.colorbar(img,cmap=cmap)
     
     cbar.set_ticks(np.arange(minn, maxx, cbar_tix))
@@ -64,4 +75,4 @@ def heatmapping(data,minn,maxx,cbar_tix,fig_width,fig_height,title='',fname=''):
     
 
 h_z=scipy.stats.zscore(h,axis=1)
-heatmapping(h_z,-2,2,1,16,8,title="Hweights",fname=sys.argv[2])    
+heatmapping(h_z,args.minimum,args.maximum+0.0001,2,args.width,args.height,title="Hweights",fname=args.output)    
