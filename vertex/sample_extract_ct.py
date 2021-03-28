304fe79275403d3b056e83e37d903c9eb492cdaf
raihaan <patelraihaan@gmail.com>
2021-03-28 13:58:16 -0400

    add parser args and reformat

--- vertex/sample_extract_ct.py
+++ vertex/sample_extract_ct.py
@@ -13,82 +13,67 @@ import sys
 import pickle
 import scipy
 from scipy.io import savemat, loadmat
+import argparse
 
-#Read in csv with subject demographics 
-df_sorted = pd.read_csv('../../raw_data/sheets/20-03-14-McGillData_WH_Exprodo-Report_IncExc_CR_CRmed_cham_CRtopfdemeduc_civetpass_sorted.csv') #MODIFY to replace the .csv filename with the path to your sorted demographics csv file
+parser=argparse.ArgumentParser(
+    description='''This script extracts voxel data from nifti files and outputs a voxel x subject matrix
+    in .mat format''')
+
+parser=argparse.ArgumentParser(
+    description='''This script extracts voxel data from nifti files and outputs a voxel x subject matrix
+    in .mat format''')
+
+parser.add_argument('--metric',type=str, nargs='+', action='append')
+parser.add_argument('--metric_column',type=str, nargs='+', action='append')
+group = parser.add_argument_group(title="Execution options")
+
+group.add_argument(
+    '--input_csv', help='demographic spreadsheet, must contain subject id',required=True)
+group.add_argument(
+    '--mask_file', help='path to CIVET midline mask',required=True)
+
+args=parser.parse_args()
+
+def load_vertex_data(df, column, n_subjects, n_vertex, mask=None):
+#helper function to cycle through array of filepaths, load data, build matrix
+    filepaths = df[[column]].values
+    for f in range(0,n_subjects):
+        fname=filepaths[f,0]
+        #print(fname)
+        if f == 0:
+            vertex_data = np.loadtxt(fname).reshape(1,n_vertex)
+        else:
+            vertex_data = np.concatenate(
+            (vertex_data, np.loadtxt(fname).reshape(1,n_vertex)), axis=0)
+    if mask is not None:
+        return vertex_data[:,mask]
+    else:
+        return vertex_data
+
+def save_mat(x,key,fname):
+    print("Saving ", np.shape(x), key, "to", fname)
+    scipy.io.savemat(fname, {'X': x})
 
 #LOAD CIVET MASK TO IDENTIFY MIDLINE/ CORPOS COLLOSUM REGION
 #IDENTIFY 'VALID VERTICES' - IE VERTICES NOT IN THIS REGION
-left_mask = np.loadtxt('../surfsamp/mask_files/CIVET_2.0_mask_left_short.txt') #MODIFY to point to your left mask file
-left_valid = np.where(left_mask==1) # list of valid indices in civet .txt file
-left_invalid = np.where(left_mask==0) #list of invalid indices in civet .txt file
+midline_mask = np.loadtxt(args.mask_file)
+valid_vertices = np.where(midline_mask==1)[0].tolist() # list of valid indices in civet .txt file
+invalid_vertices = np.where(midline_mask==0)[0].tolist() #list of invalid indices in civet .txt file
 
-right_mask = np.loadtxt('../surfsamp/mask_files/CIVET_2.0_mask_right_short.txt') #MODIFY to point to your right mask file
-right_valid = (np.where(right_mask==1))
-right_invalid = (np.where(right_mask==0))
-#38561 valid vertices
+df_inputs = pd.read_csv(args.input_csv)
+n_subjects=df_inputs.shape[0] #num rows in spreadsheet
+n_vertex=np.shape(midline_mask)[0]
 
-n_subjects=df_sorted.shape[0] #num rows in spreadsheet
-n_vertex=40962
-
-#Load left thicknesses into matrix left_ct
-#do first row to create matrix, concat from there
-row = df_sorted['oxmg_id'].tolist()[0] #MODIFY replace oxmg_id with the name of the column that contains subject id in your spreadsheet
-fname="../civet/thickness/oxmg_" + str(row) + '_Struct_N_defaced_native_rms_rsl_tlaplace_30mm_left.txt' #MODIFY paths so that this evaluates to your civet thickness files
-left_ct = np.loadtxt(fname).reshape(1,n_vertex)
-print(np.shape(left_ct)) #1 x 40962
-
-#load thickness file for rest of subjects and concatenate to make subjects X vertices matrix
-for row in df_sorted['oxmg_id'].tolist()[1:]: #MODIFY replace oxmg_id with the name of the column that contains subject id in your spreadsheet
-    fname="../civet/thickness/oxmg_" + str(row) + '_Struct_N_defaced_native_rms_rsl_tlaplace_30mm_left.txt' #MODIFY paths so that this evaluates to your civet thickness files
-    x = np.loadtxt(fname).reshape(1,n_vertex)
-    left_ct = np.concatenate((left_ct, x), axis=0)
-print("raw left has", np.shape(left_ct)[0], "subjects", np.shape(left_ct)[1], "vertices")
-
-#Repeat for right side
-row = df_sorted['oxmg_id'].tolist()[0] #MODIFY replace oxmg_id with the name of the column that contains subject id in your spreadsheet
-fname="../civet/thickness/oxmg_" + str(row) + '_Struct_N_defaced_native_rms_rsl_tlaplace_30mm_right.txt' #MODIFY paths so that this evaluates to your civet thickness files
-right_ct = np.loadtxt(fname).reshape(1,n_vertex)
-print(np.shape(right_ct)) #1 x 40962
-
-#load thickness file for rest of subjects and concatenate to make subjects X vertices matrix
-for row in df_sorted['oxmg_id'].tolist()[1:]: #MODIFY replace oxmg_id with the name of the column that contains subject id in your spreadsheet
-    fname="../civet/thickness/oxmg_" + str(row) + '_Struct_N_defaced_native_rms_rsl_tlaplace_30mm_right.txt' #MODIFY paths so that this evaluates to your civet thickness files
-    x = np.loadtxt(fname).reshape(1,n_vertex)
-    right_ct = np.concatenate((right_ct, x), axis=0)
-print("raw right has", np.shape(right_ct)[0], "subjects", np.shape(right_ct)[1], "vertices")
-
-#valid ct matrices have dimensions subj x validvertices, masked midline vertices removed
-n_rows = np.shape(left_ct)[0] #same for right side
-n_cols = np.shape(left_valid)[1] #same for right side
-
-#take all rows (subjects), but only columns which are valid vertices
-left_ct_valid = left_ct[:, left_valid].reshape(n_rows,n_cols)
-right_ct_valid = right_ct[:, right_valid].reshape(n_rows,n_cols)
-
-
-#write out in .mat format
-#nmf will want vertex X subj, so transpose
-out_matrix = np.transpose(np.asmatrix(left_ct_valid))
-fname = "left_ct_raw.mat"
-print("Saving raw left ct to", fname,  "with shape", np.shape(out_matrix))
-scipy.io.savemat(fname, {'X': out_matrix})
-del out_matrix
-
-out_matrix = np.transpose(np.asmatrix(right_ct_valid))
-fname = "right_ct_raw.mat"
-print("Saving raw right ct to", fname, "with shape", np.shape(out_matrix))
-scipy.io.savemat(fname, {'X': out_matrix})
-del out_matrix
-
-#Concatenate the left and right hemisphere data to get whole brain data
-wb_ct_valid = np.concatenate((np.transpose(left_ct_valid), np.transpose(right_ct_valid)),axis=0)
-
-#write out whole brain data in .mat format
-#write out z scored, un shifted data for stability analysis (to be shifted within each split later)
-out_matrix = np.asmatrix(wb_ct_valid)
-fname = "wholebrain_ct_raw.mat"
-print("Saving whole brain ct to", fname, "with shape", np.shape(out_matrix))
-scipy.io.savemat(fname, {'X': out_matrix})
-
-
+metric_dict = {}
+for m_idx,m in enumerate(args.metric):
+    metric = m[0] #args.metric is list of lists, each w length 1. we just want the metric, not whole list
+    print('extracting', metric)
+    for c_idx,c in enumerate(args.metric_column[m_idx]):
+        vertex_data = load_vertex_data(df_inputs,c,n_subjects,n_vertex, valid_vertices)
+        save_mat(np.transpose(vertex_data), c, c + '.mat')
+        if c_idx == 0:
+            metric_dict[metric] = np.transpose(vertex_data.copy())
+        else:
+            metric_dict[metric] = np.concatenate(
+            (metric_dict[metric], np.transpose(vertex_data.copy())), axis=0)
+    save_mat(metric_dict[metric], 'wb_' + metric, 'wb_' + metric + '.mat')
